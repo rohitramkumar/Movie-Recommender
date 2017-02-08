9f04b692fdfad3944ea14af6e53be29280fc5c47
rohitramkumar <rohit.ramkumar@yahoo.com>
2017-02-07 19:57:39 -0600

    Blah

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello, World!"
+    return "Hello, World!!"
 
 if __name__ == '__main__':
     app.run(debug=False, port='8888', host='0.0.0.0')
--- push_script.sh
+++ push_script.sh
@@ -2,4 +2,4 @@
 
 git add .
 git commit -m "Blah"
-git push heroku master
+git push origin master
