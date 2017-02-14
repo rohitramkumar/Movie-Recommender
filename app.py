4997d57a7d32ba353f26e117f7e50187d3b9462f
Hassan <hassan@gmail.com>
2017-02-13 21:49:03 -0600

    Testing 8

--- app.py
+++ app.py
@@ -5,9 +5,17 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index():
-    return send_file("templates/index.html")
+    response = app.make_response(send_file("templates/index.html"))
+    response.headers['Last-Modified'] = datetime.now()
+    response.headers[
+        'Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+
+    return response
 
 if __name__ == '__main__':
     app.run(debug=False, port='8888', host='0.0.0.0')
