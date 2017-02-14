a66aaaaa433e0e69622c79bf1d2ae6ee579b4751
Hassan <hassan@gmail.com>
2017-02-13 21:50:45 -0600

    testing 89

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ app = Flask(__name__)
 def index():
     response = app.make_response(send_file("templates/index.html"))
     response.headers['Last-Modified'] = datetime.now()
-    response.headers[
-        'Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '-1'
 
