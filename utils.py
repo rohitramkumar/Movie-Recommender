89036ce0566998edc9379d4f00915fd82e9b8158
rohitramkumar <rohit.ramkumar@yahoo.com>
2017-02-28 18:19:06 -0600

    Added a layer of abstraction where all API processing code is in a seperate class. Makes things slightly cleaner

---
+++ utils.py
@@ -0,0 +1,71 @@
+import requests
+import json
+import urllib
+import os
+
+class MovieDBApiClient:
+
+  API_KEY = os.environ['MOVIE_DB_API_KEY']
+  # Database that provides simple filtering.
+  MOVIE_DB_URL = 'https://api.themoviedb.org/3/'
+  # URL Endpoints for different types of filtering data.
+  GENRES_URL = (MOVIE_DB_URL + 'genre/movie/list?api_key={}&language=en-US').format(API_KEY)
+  PEOPLE_SEARCH_URL = (MOVIE_DB_URL + 'search/person/?api_key={}&language=en-US&query={}&page1&include_adult=false')
+  # URL Endpoint for movie discovery
+  MOVIE_DISCOVERY_URL = (MOVIE_DB_URL + 'discover/movie?api_key={}&include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc').format(API_KEY)
+  # TODO: maybe make this user configurable
+  MAX_RESULTS = 5
+
+  def __init__(self):
+      pass
+
+  def getGenresIds(self, userSpecifiedGenres):
+    genreRequestResult = requests.get(GENRES_URL)
+    genres = json.loads(genreRequestResult.text)
+    # For each user specified genre, find its corresponding genre id
+    genreIds = []
+    for userSpecifiedGenre in userSpecifiedGenres:
+      for genre in genres['genres']:
+        if userSpecifiedGenre == genre['name']:
+          genreIds.append(genre['id'])
+    return genreIds
+
+  def getCastIds(self, userSpecifiedCast):
+    for cast in userSpecifiedCast:
+      castRequestResult = requests.get(PEOPLE_SEARCH_URL.format(API_KEY, urllib.quote_plus(cast)))
+      castInfo = json.loads(castRequestResult.text)
+      if len(castInfo.get('results')) > 0:
+        castIds.append(castInfo.get('results')[0].get('id'))
+    return castIds
+
+  def getDiscoveredMovies(self, discoveryURL):
+    movieDiscoveryRequest = requests.get(discoveryURL)
+    movieDiscoveryResults = json.loads(movieDiscoveryRequest.text)
+    movies = []
+    counter = 0
+    while counter < MAX_RESULTS and counter < len(movieDiscoveryResults.get('results')):
+      recommendations.append(movieDiscoveryResults.get('results')[counter].get('title'))
+      counter += 1
+    return movies
+
+  def getSimilarMovies(self, benchmarkMovie):
+      pass
+
+  def encodeURLKeyValue(self, pair):
+    if len(pair[1]) == 0 or len(pair[0]) == 0:
+        return ""
+    else:
+      if type(pair[1]) == list:
+        return '&' + pair[0] + '=' + ''.join(str(i) for i in pair[1])
+      elif type(pair[1]) == str:
+        return '&' + pair[0] + '=' + pair[1]
+      else:
+        return ""
+
+class LearningAgentClient:
+
+    def __init__(self, user):
+        self.user = user
+
+    def getRecommendedMovies(self):
+        pass
