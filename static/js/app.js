b3c183e18f4e850653ce3cbf152c99f1b9607dee
Hassan <hassan@gmail.com>
2017-03-03 19:00:05 -0600

    Adding ui router

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,41 @@
 'use strict';   // See note about 'use strict'; below
 
-var myApp = angular.module('myApp', [
- 'ngRoute',
+var movieApp = angular.module('myApp', [
+ 'ui.router',
 ]);
 
-myApp.config(['$routeProvider',
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'static/partials/partial-home.html'
+        })
+
+        // nested list with custom controller
+        .state('home.movie_detail', {
+            url: '/movie_detail',
+            templateUrl: 'partial-movie-detail.html',
+            controller: function($scope) {
+                        $scope.movies = ['Die Hard', 'Star Wars', 'The Hobbit'];
+            }
+        })
+
+        // nested list with just some random string data
+        .state('home.paragraph', {
+            url: '/paragraph',
+            template: 'I could sure use a glass of water right now.'
+        })
+
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('about', {
+            // we'll get to this in a bit
+        });
+
+});
+
+/*movieApp.config(['$routeProvider',
      function($routeProvider) {
          console.log("I'm trying to resolve routes!");
          $routeProvider.
@@ -21,3 +52,4 @@ myApp.config(['$routeProvider',
                  redirectTo: '/'
              });
     }]);
+*/
