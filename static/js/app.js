58417672e17acd4c725eacafa66022f85d46da42
Hassan <hassan@gmail.com>
2017-03-04 02:36:36 -0600

    Fixing footer

--- static/js/app.js
+++ static/js/app.js
@@ -19,7 +19,7 @@ movieApp.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: 'static/partials/partial-movie-detail.html',
             controller: function($scope, $rootScope) {
                         $scope.movies = ['Die Hard', 'Star Wars', 'The Hobbit'];
-                        $rootScope.userIsLoggedIn = true
+                        $rootScope.userIsLoggedIn = true;
             }
         })
 
