1f203b795db8af6190635246f8ce86d372a2c9f0
Hassan <hassan@gmail.com>
2017-04-05 19:02:43 -0500

    now 1

--- static/js/app.js
+++ static/js/app.js
@@ -97,9 +97,9 @@ movieApp.config(function($stateProvider, $urlRouterProvider) {
                             }
 
                             function setResponse(val) {
-                                var respObject = JSON.parse(val)
+                                var respObject = JSON.parse(val);
                                 console.log(respObject);
-                                $("#response").val(respObject.data.fulfillment.speech);
+                                $("#response").val(respObject.result.fulfillment.speech);
                             }
 
                         }
