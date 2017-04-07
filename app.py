246b0dd9ee1d93940407ec2f02a757d1f6e7fcaf
rohitramkumar <rohitramkumar13@gmail.com>
2017-04-06 22:06:33 -0500

    Added some tests and cleanup of some code

--- app.py
+++ app.py
@@ -11,30 +11,32 @@ import json
 import sys
 import logging
 
+# App and database config.
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 Bootstrap(app)
 db = SQLAlchemy(app)
 
-
 @app.route("/")
 def index():
     return render_template('index.html')
 
-
 @app.route("/api/login/", methods=['POST'])
 def login():
+    """Api endpoint which handles login authentication for the frontend."""
 
     user_detail = json.loads(request.data)
     username = user_detail.get("username")
     password = user_detail.get("password")
     return jsonify(utils.login(username, password))
 
-
 @app.route("/api/signup/", methods=['POST'])
 def signup():
+    """Api endpoint which handles user signup."""
+
     new_user = json.loads(request.data)
     first_name = new_user.get("firstname")
     last_name = new_user.get("lastname")
@@ -42,34 +44,33 @@ def signup():
     password = new_user.get("password")
     return utils.createUser(username, password, first_name, last_name)
 
-
 @app.route("/api/add_movie/", methods=['POST'])
 def add_movie():
-    """This function triggers an API call to add a new
-    movie into the watchlist for a user"""
+    """Api endpoint which adds a new movie into the watchlist for a user."""
+
     movie_detail = json.loads(request.data)
-
     username = movie_detail.get("username")
-    movieName = movie_detail.get("movieName")
-    movieImdbID = movie_detail.get("movieImdbId")
-    movieRating = movie_detail.get("movieRating")
-
-    return utils.add_movie(username, movieName, movieImdbID, movieRating)
-
+    movie_name = movie_detail.get("movieName")
+    movie_imdb_id = movie_detail.get("movieImdbId")
+    movie_rating = movie_detail.get("movieRating")
+    return utils.add_movie(username, movie_name, movie_imdb_id, movie_rating)
 
 @app.route("/api/get_movie_all/", methods=['POST'])
 def get_all_movies():
-    """This function triggers an API call to add a new
-    movie into the watchlist for a user"""
-    user_id = request.data
+    """Api endpoint which gets all movies in a user's watchlist."""
 
+    user_id = request.data
     return jsonify(utils.get_movie_all(user_id))
 
+@app.route("/api/get_learning_recommendation/", methods=['POST'])
+def get_learning_recommendation():
+    """Api endpoint which gets a movie recommendation based on a user's watchlist."""
+    pass
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    """This Flask route receives all requests from API.ai and processes these
-    requests according to the action specified by the user"""
+    """Receives all requests from API.ai and processes these
+    requests according to the action specified by the user."""
 
     req = request.get_json(force=True)
     action = req.get('result').get('action')
@@ -80,14 +81,12 @@ def webhook():
         res = processSimilarityRequest(req)
     r = make_response(json.dumps(res))
     r.headers['Content-Type'] = 'application/json'
-
     return r
 
-
 def processFilteringRequest(req):
-    """This function deals with processing the movie filters provided by a user
-    and feeding these filters into api calls for a movie database. The filtered
-    movies returned from these api calls are returned to the user."""
+    """Deals with processing the movie filters provided by a user and feeding
+    these filters into api calls for the movie database. The filtered movies
+    returned from these api calls are returned to the user."""
 
     userSpecifiedData = req.get('result').get('contexts')[0].get('parameters')
     maxResults = int(userSpecifiedData.get('max-results'))
@@ -119,35 +118,41 @@ def processFilteringRequest(req):
     movieDetails = client.getMovieDetails(movies)
     return prepareResponse(movies, movieDetails, "gathered-filters", maxResults + totalResultsGiven)
 
-
 def processSimilarityRequest(req):
-    """The function deals with processing a single movie provided by the user and
-    returning a list of movies which are similar."""
-    client = utils.MovieDBApiClient()
-    benchmarkMovie = req.get('result').get('contexts')[0].get('parameters').get('benchmark')
+    """Deals with processing a single movie provided by the user and returning
+    a list of movies which are similar."""
+
+    userSpecifiedData = req.get('result').get('contexts')[0].get('parameters')
+    maxResults = int(userSpecifiedData.get('max-results'))
+    totalResultsGiven = int(userSpecifiedData.get('total-results-given'))
+    client = utils.MovieDBApiClient(maxResults, totalResultsGiven)
+    benchmarkMovie = userSpecifiedData.get('benchmark')
     benchmarkMovie = utils.spellCheck(benchmarkMovie)
     similarMovies = client.getSimilarMovies(benchmarkMovie)
     movieDetails = client.getMovieDetails(similarMovies)
     return prepareResponse(similarMovies, movieDetails, "gathered-benchmark-movie")
 
-
 def prepareResponse(movies, movieDetails, outboundContextName, outboundContextParam):
     """Helper function that prepares the return object we send to the user
      given a list of movies."""
+
     if len(movies) > 0:
         speech = "I found you the following movies: " + ', '.join(movies)
     else:
         speech = "Sorry, no movies available."
     return {
         "speech": speech,
-      "displayText": speech,
-      "source": "movie-recommendation-service",
-      "contextOut": [
-          {"name": outboundContextName, "parameters":
-              {"total-results-given": outboundContextParam}, "lifespan": 1}
-      ],
-      "data": movieDetails
+        "displayText": speech,
+        "source": "movie-recommendation-service",
+        "contextOut": [
+            {
+            "name": outboundContextName,
+            "parameters": {"total-results-given": outboundContextParam}, "lifespan": 1
+            }
+        ],
+        "data": movieDetails
     }
 
 if __name__ == '__main__':
+    # For local debugging.
     app.run(debug=True, port=8888, host='0.0.0.0')
--- tests.py
+++ tests.py
@@ -1,14 +1,18 @@
+import json
 import unittest
 import mock
 from app import processFilteringRequest, processSimilarityRequest, prepareResponse
 
 class AppTest(unittest.TestCase):
 
-  def processFilteringRequestTest(self):
-    pass
+    def test_process_filtering_request(self):
+        with open("test_json/filtering_req.json", "r") as f:
+            j = json.loads(f.read())
+            result = processFilteringRequest(j)
+            self.assertEqual(result["speech"], "I found you the following movies: Mad Max: Fury Road, The Dark Knight Rises")
 
-  def processSimilarityRequestTest(self):
-    pass
+    def test_process_similarity_request(self):
+        pass
 
-  def prepareResponseTest(self):
-    pass
+if __name__ == "__main__":
+    unittest.main()
