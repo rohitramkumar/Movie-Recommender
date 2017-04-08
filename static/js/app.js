8e61183b0cd7922d6fd90c1083faad5db87c8738
Amanda Thomas <amthoma3@illinois.edu>
2017-04-07 23:37:16 -0500

    forward and back buttons fully functional

--- static/js/app.js
+++ static/js/app.js
@@ -110,7 +110,33 @@ movieApp.config(function($stateProvider, $urlRouterProvider) {
                                 if (respStr.includes("I found you the following movies")) {
                                     console.log("Display movie details!!");
                                     console.log(respObject.result.fulfillment.data);
-                                    $scope.movies = respObject.result.fulfillment.data;
+                                    var movie_arr = respObject.result.fulfillment.data;
+
+
+                                    var index = 0;
+                                    $scope.movies = movie_arr;
+                                    $scope.movies.currentMovie = movie_arr[index];
+                                    $scope.movies.nextMovie = function() {
+                                        if(index >= (movie_arr.length - 1)){
+                                            index = 0;
+                                        }
+                                        else{
+                                            index = index+1;
+                                        }
+
+                                        $scope.movies.currentMovie = movie_arr[index];
+                                    };
+                                    $scope.movies.prevMovie = function() {
+                                        if(index <= 1 ){
+                                            index = movie_arr.length - 1;
+                                        }
+                                        else{
+                                            index = index-1;
+                                        }
+
+                                        $scope.movies.currentMovie = movie_arr[index];
+                                    };
+
                                     $state.go('root.home.movie_detail');
                                 }
 
