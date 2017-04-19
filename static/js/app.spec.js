e9e664c496d455f5665ce2e1d52f683e872a4d81
Zhaoli Wen <zwen6@illinois.edu>
2017-04-19 17:12:32 -0500

    jasmine setup

---
+++ static/js/app.spec.js
@@ -0,0 +1,58 @@
+describe('States', function () {
+    // Define global references for injections
+
+	beforeEach(module('myApp'));
+	beforeEach(module('ui.router'));
+
+    var $state,
+        $rootScope,
+        state = 'root.home';
+
+    // Inject and assign the $state and $rootScope services.
+    // Put the template in template cache.
+    beforeEach(inject(function (_$state_, $templateCache, _$rootScope_) {
+        $state = _$state_;
+        $rootScope = _$rootScope_;
+
+        $templateCache.put('static/partials/partial-home.html', '');
+        $templateCache.put('static/partials/partial-header.html', '');
+        $templateCache.put('static/partials/partial-footer.html', '');
+        $templateCache.put('static/partials/layout.html', '');
+        $templateCache.put('static/partials/partial-movie-detail.html', '');
+        $templateCache.put('static/partials/partial-login.html', '');
+    }));
+
+    // Test whether the url is correct
+    it('root URL', function() {
+        // expect($state.href(state)).toEqual('root.home');
+    });
+
+    // Test whether our state activates correctly
+    it('root.home state activation', function() {
+        $state.go(state, {}, {reload: true});
+        $rootScope.$digest();
+        // expect($state.href(state)).toEqual('root.home');
+        expect($state.current.name).toBe(state);
+        expect($state.current.name).toBe('root.home');
+    });
+
+    // Test whether our state activates correctly
+    it('root.home state activation', function() {
+
+        $state.go(state, {}, {reload: true});
+        $rootScope.$digest();
+        // expect($state.href(state)).toEqual('root.home');
+        expect($state.current.name).toBe(state);
+        expect($state.current.name).toBe('root.home');
+
+        $state.go('root.home.movie_detail');
+        $rootScope.$digest();
+        expect($state.current.name).toBe('root.home.movie_detail');
+
+        $state.go('login');
+        $rootScope.$digest();
+        expect($state.current.name).toBe('login');
+
+    });
+});
+
