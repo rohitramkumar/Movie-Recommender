8046c642445e5ec1aa8759d4ff8cf9dae439b71b
rohitramkumar <rohitramkumar13@gmail.com>
2017-04-26 16:04:08 -0500

    Some refactoring and pep8 changes

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import os
 import model
 import time
 
+# API Keys
 GUIDEBOX_API_KEY = os.environ['GUIDEBOX_API_KEY']
 BING_SC_API_KEY = os.environ['BING_SC_API_KEY']
 MOVIE_DB_API_KEY = os.environ['MOVIE_DB_API_KEY']
@@ -12,19 +13,26 @@ MOVIE_DB_API_KEY = os.environ['MOVIE_DB_API_KEY']
 GUIDEBOX_MOVIE_SEARCH_URL = 'http://api-public.guidebox.com/v2/search?api_key={}&type=movie&field=title&query={}'
 # URL endpoint which provides info about a movie given a guidebox id
 GUIDEBOX_MOVIE_INFO_URL = 'http://api-public.guidebox.com/v2/movies/{}?api_key={}'
-# Database that provides simple filtering.
+# Movie database that provides simple filtering
 MOVIE_DB_URL = 'https://api.themoviedb.org/3/'
-# URL Endpoints for different types of filtering data.
-GENRES_URL = (MOVIE_DB_URL + 'genre/movie/list?api_key={}&language=en-US').format(MOVIE_DB_API_KEY)
+# URL Endpoints for different types of filtering data
+GENRES_URL = (
+    MOVIE_DB_URL +
+    'genre/movie/list?api_key={}&language=en-US').format(MOVIE_DB_API_KEY)
 PEOPLE_SEARCH_URL = (
-    MOVIE_DB_URL + 'search/person?api_key={}&language=en-US&query={}&page=1&include_adult=false')
+    MOVIE_DB_URL +
+    'search/person?api_key={}&language=en-US&query={}&page=1&include_adult=false')
 MOVIE_SEARCH_URL = (
-    MOVIE_DB_URL + 'search/movie?api_key={}&language=en-US&query={}&page=1&include_adult=false')
+    MOVIE_DB_URL +
+    'search/movie?api_key={}&language=en-US&query={}&page=1&include_adult=false')
 # URL Endpoint for movie discovery
 MOVIE_DISCOVERY_URL = (
-    MOVIE_DB_URL + 'discover/movie?api_key={}&include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc').format(MOVIE_DB_API_KEY)
+    MOVIE_DB_URL +
+    'discover/movie?api_key={}&include_adult=false&include_video=false&language=en-US&sort_by=popularity.desc').format(MOVIE_DB_API_KEY)
 # URL Endpoint for movie similarity
-MOVIE_SIMILARITY_URL = (MOVIE_DB_URL + 'movie/{}/similar?api_key={}&language=en-US')
+MOVIE_SIMILARITY_URL = (
+    MOVIE_DB_URL +
+    'movie/{}/similar?api_key={}&language=en-US')
 # URL Endpoint for movie info
 MOVIE_INFO_URL = (MOVIE_DB_URL + 'movie/{}?api_key={}&language=en-US')
 # URL Endpoint for movie info (credits)
@@ -41,8 +49,8 @@ LEARNING_AGENT_HIST_URL = "https://52.165.149.158/mrelearner/api/v1.0/history"
 MAX_RESULTS = 10
 
 
-def createUser(username, password, first_name, last_name):
-    """Used for sign-up. Gets form data and adds new user to users table"""
+def create_user(username, password, first_name, last_name):
+    """Used for sign-up. Gets form data and adds new user to users table."""
 
     if not password:
         return "Cannot leave password empty"
@@ -51,7 +59,7 @@ def createUser(username, password, first_name, last_name):
     # Check if user exists
     user = model.User.query.filter_by(email=username).first()
     # If user doesn't exist, create user
-    if user == None:
+    if user is None:
         user = model.User(email=username, password=password,
                           first_name=first_name, last_name=last_name)
         model.session.add(user)
@@ -83,181 +91,227 @@ def get_watchlist(username):
     return movie_list
 
 
-def add_movie_to_watchlist(username, userID, movieName, movieImdbId, movieRating):
+def add_movie_to_watchlist(
+        username,
+        user_id,
+        movie_name,
+        movie_imdb_id,
+        movie_rating):
     """Check if user exists; if exists, add movie to specified user's watchlist.
     Also add movie to learning agent database"""
 
     user = model.User.query.filter_by(email=username).first()
     if not user:
         return "Fail: Cannot find user!"
-    newMovie = model.Movie(name=movieName, movie_imdb_id=movieImdbId, user_rating=movieRating)
+    new_movie = model.Movie(
+        name=movie_name,
+        movie_imdb_id=movie_imdb_id,
+        user_rating=movie_rating)
     for movie in user.movies:
-        if movie.name == movieName:
+        if movie.name == movie_name:
             return "Movie already present in watchlist!"
-    user.movies.append(newMovie)
+    user.movies.append(new_movie)
     model.session.commit()
     # Learning Agent watchlist.
     client = LearningAgentClient()
-    client.addMovieToUserHistory({'user_id': userID,
-                                  'movie_imdb_id': movieImdbId,
-                                  'user_rating': movieRating,
-                                  'timestamp': int(time.time())})
+    client.add_movie_to_user_history({'user_id': user_id,
+                                      'movie_imdb_id': movie_imdb_id,
+                                      'user_rating': movie_rating,
+                                      'timestamp': int(time.time())})
     return "Success"
 
 
-def spellCheck(query):
+def spell_check(query):
     """Given a potentially misspelled query, return the correct spelling with
     the proper punctuation, capitalization, etc if necessary. Uses the Bing
     Spell Check API."""
 
-    newQuery = query
-    encodedQuery = urllib.quote_plus(query)
-    url = BING_SC_URL + '&text=' + encodedQuery
+    new_query = query
+    encoded_query = urllib.quote_plus(query)
+    url = BING_SC_URL + '&text=' + encoded_query
     headers = {'Content-Type': 'application/x-www-form-urlencoded',
                'Ocp-Apim-Subscription-Key': BING_SC_API_KEY}
-    spellCheckResponse = requests.post(url, headers=headers)
-    spellCheckInfo = json.loads(spellCheckResponse.text)
-    for flaggedToken in spellCheckInfo.get('flaggedTokens'):
-        token = flaggedToken.get('token')
-        suggestion = flaggedToken.get('suggestions')[0].get('suggestion')
-        newQuery = newQuery.replace(token, suggestion)
-    return newQuery
+    spell_check_response = requests.post(url, headers=headers)
+    spell_check_info = json.loads(spell_check_response.text)
+    for flagged_token in spell_check_info.get('flaggedTokens'):
+        token = flagged_token.get('token')
+        suggestion = flagged_token.get('suggestions')[0].get('suggestion')
+        new_query = new_query.replace(token, suggestion)
+    return new_query
 
 
 def get_guidebox_info(movie_names):
+    """Given a list of movie names, return the Guidebox information for each.
+    Guidebox provides metacritic links and links to streaming services."""
+
     guidebox_info = {}
     for movie in movie_names:
-        movie_id_result = requests.get(GUIDEBOX_MOVIE_SEARCH_URL.format(GUIDEBOX_API_KEY, movie)).json()
+        movie_id_result = requests.get(
+            GUIDEBOX_MOVIE_SEARCH_URL.format(
+                GUIDEBOX_API_KEY, movie)).json()
         if len(movie_id_result.get('results')) == 0:
             guidebox_info[movie] = "No info"
             continue
         movie_id = movie_id_result.get('results')[0].get('id')
-        guidebox_info_result = requests.get(GUIDEBOX_MOVIE_INFO_URL.format(movie_id, GUIDEBOX_API_KEY)).json()
-        # If the movie is in theaters, then provide the fandango link and the metacritic link.
-        if guidebox_info_result.get('in_theaters') == True:
+        guidebox_info_result = requests.get(
+            GUIDEBOX_MOVIE_INFO_URL.format(
+                movie_id, GUIDEBOX_API_KEY)).json()
+        # If the movie is in theaters, then provide the fandango link and the
+        # metacritic link.
+        if guidebox_info_result.get('in_theaters') is True:
             fandango = None
             other_sources = guidebox_info_result.get('other_sources')
             if 'movie_theater' in other_sources:
                 for source in other_sources:
                     if source.get('source') == 'fandango':
                         fandango = source.get('link')
-                        guidebox_info[movie] = {'metacritic' : guidebox_info_result.get('metacritic'), 'fandango' : fandango}
+                        guidebox_info[movie] = {
+                            'metacritic': guidebox_info_result.get('metacritic'),
+                            'fandango': fandango}
             else:
-                guidebox_info[movie] = {'metacritic' : guidebox_info_result.get('metacritic')}
-        # If the movie is not in theatres, provide the metacritic link and a list of streaming options, if applicable.
+                guidebox_info[movie] = {
+                    'metacritic': guidebox_info_result.get('metacritic')}
+        # If the movie is not in theatres, provide the metacritic link and a
+        # list of streaming options, if applicable.
         else:
-            subscription_web_sources = guidebox_info_result.get('subscription_web_sources')
+            subscription_web_sources = guidebox_info_result.get(
+                'subscription_web_sources')
             streaming = []
             for source in subscription_web_sources:
-                streaming.append({'source' : source.get('source'), 'link' : source.get('link')})
-            purchase_web_sources = guidebox_info_result.get('purchase_web_sources')
+                streaming.append({'source': source.get(
+                    'source'), 'link': source.get('link')})
+            purchase_web_sources = guidebox_info_result.get(
+                'purchase_web_sources')
             for source in purchase_web_sources:
-                streaming.append({'source' : source.get('source'), 'link' : source.get('link')})
-            guidebox_info[movie] = {'metacritic' : guidebox_info_result.get('metacritic'), 'streaming' : streaming}
+                streaming.append({'source': source.get(
+                    'source'), 'link': source.get('link')})
+            guidebox_info[movie] = {
+                'metacritic': guidebox_info_result.get('metacritic'),
+                'streaming': streaming}
     return guidebox_info
+
 
 class MovieDBApiClient:
 
     """This class abstracts API calls for the api.themoviedb.org."""
 
-    def __init__(self, maxResults, offset):
-        self.maxResults = maxResults if maxResults > 0 and maxResults < MAX_RESULTS else MAX_RESULTS
+    def __init__(self, max_results, offset):
+        self.max_results = max_results if max_results > 0 and max_results < MAX_RESULTS else MAX_RESULTS
         self.offset = offset
 
-    def getMovieDetails(self, movieList):
+    def get_movie_details(self, movie_list):
         """Gets the imdb id, cast, title, overview and picture for each movie."""
-        movieIds = []
-        for movie in movieList:
-            movieIdResult = requests.get(
-                MOVIE_SEARCH_URL.format(MOVIE_DB_API_KEY, urllib.quote_plus(movie)))
-            movieIdInfo = json.loads(movieIdResult.text)
-            if len(movieIdInfo.get('results')) > 0:
-                movieId = movieIdInfo.get('results')[0].get('id')
-                movieIds.append(movieId)
-        fullMovieDetails = []
+        movie_ids = []
+        for movie in movie_list:
+            movie_id_result = requests.get(
+                MOVIE_SEARCH_URL.format(
+                    MOVIE_DB_API_KEY,
+                    urllib.quote_plus(movie)))
+            movie_id_info = json.loads(movie_id_result.text)
+            if len(movie_id_info.get('results')) > 0:
+                movie_id = movie_id_info.get('results')[0].get('id')
+                movie_ids.append(movie_id)
+        full_movie_details = []
         counter = 0
-        for movieId in movieIds:
-            fullMovieDetails.append({})
-            movieInfoResult = requests.get(MOVIE_INFO_URL.format(movieId, MOVIE_DB_API_KEY))
-            castInfoResult = requests.get(MOVIE_CREDITS_URL.format(movieId, MOVIE_DB_API_KEY))
-            movieInfo = json.loads(movieInfoResult.text)
-            castInfo = json.loads(castInfoResult.text)
-            imdb_id_str = movieInfo['imdb_id'][2:]
-            fullMovieDetails[counter]['imdb_id'] = imdb_id_str
-            fullMovieDetails[counter]['overview'] = movieInfo['overview']
-            fullMovieDetails[counter]['original_title'] = movieInfo['original_title']
-            fullMovieDetails[counter]['poster'] = MOVIE_POSTER_URL + movieInfo['poster_path']
-            fullMovieDetails[counter]['cast'] = [item['name'] for item in castInfo['cast'][:5]]
-            fullMovieDetails[counter]['release_date'] = movieInfo['release_date']
+        for movie_id in movie_ids:
+            full_movie_details.append({})
+            movie_info_result = requests.get(
+                MOVIE_INFO_URL.format(
+                    movie_id, MOVIE_DB_API_KEY))
+            cast_info_result = requests.get(
+                MOVIE_CREDITS_URL.format(
+                    movie_id, MOVIE_DB_API_KEY))
+            movie_info = json.loads(movie_info_result.text)
+            cast_info = json.loads(cast_info_result.text)
+            imdb_id_str = movie_info['imdb_id'][2:]
+            full_movie_details[counter]['imdb_id'] = imdb_id_str
+            full_movie_details[counter]['overview'] = movie_info['overview']
+            full_movie_details[counter]['original_title'] = movie_info['original_title']
+            full_movie_details[counter]['poster'] = MOVIE_POSTER_URL + \
+                movie_info['poster_path']
+            full_movie_details[counter]['cast'] = [item['name']
+                                                   for item in cast_info['cast'][:5]]
+            full_movie_details[counter]['release_date'] = movie_info['release_date']
             counter += 1
-        return fullMovieDetails
+        return full_movie_details
 
-    def getGenresIds(self, userSpecifiedGenres):
+    def get_genre_ids(self, user_specified_genres):
         """Gets a list of genre id's corresponding to the names of the genres
         specified by the user."""
 
-        genreRequestResult = requests.get(GENRES_URL)
-        genres = json.loads(genreRequestResult.text)
+        genre_request_result = requests.get(GENRES_URL)
+        genres = json.loads(genre_request_result.text)
         # For each user specified genre, find its corresponding genre id
-        genreIds = []
-        for userSpecifiedGenre in userSpecifiedGenres:
+        genre_ids = []
+        for user_specified_genre in user_specified_genres:
             for genre in genres['genres']:
-                if userSpecifiedGenre == genre['name']:
-                    genreIds.append(genre['id'])
-        return genreIds
+                if user_specified_genre == genre['name']:
+                    genre_ids.append(genre['id'])
+        return genre_ids
 
-    def getCastIds(self, userSpecifiedCast):
+    def get_cast_ids(self, user_specified_cast):
         """Gets a list of cast (actor) id's corresponding to the names of the
         cast specified by the user."""
 
-        castIds = []
-        for cast in userSpecifiedCast:
-            castRequestResult = requests.get(
-                PEOPLE_SEARCH_URL.format(MOVIE_DB_API_KEY, urllib.quote_plus(cast)))
-            castInfo = json.loads(castRequestResult.text)
-            if len(castInfo.get('results')) > 0:
-                castIds.append(castInfo.get('results')[0].get('id'))
-        return castIds
+        cast_ids = []
+        for cast in user_specified_cast:
+            cast_request_result = requests.get(
+                PEOPLE_SEARCH_URL.format(
+                    MOVIE_DB_API_KEY,
+                    urllib.quote_plus(cast)))
+            cast_info = json.loads(cast_request_result.text)
+            if len(cast_info.get('results')) > 0:
+                cast_ids.append(cast_info.get('results')[0].get('id'))
+        return cast_ids
 
-    def getDiscoveredMovies(self, discoveryURL):
+    def get_discovered_movies(self, discovery_url):
         """Given an API endpoint which corresponds to movie discovery given a set
         of filters, get a list of movies that are returned."""
 
-        movieDiscoveryResult = requests.get(discoveryURL)
-        movieInfo = json.loads(movieDiscoveryResult.text)
+        movie_discovery_result = requests.get(discovery_url)
+        movie_info = json.loads(movie_discovery_result.text)
         movies = []
         counter = 0
-        while counter < self.maxResults and counter + self.offset < len(movieInfo.get('results')):
-            movies.append(movieInfo.get('results')[counter + self.offset].get('title'))
+        while counter < self.max_results and counter + \
+                self.offset < len(movie_info.get('results')):
+            movies.append(
+                movie_info.get('results')[
+                    counter +
+                    self.offset].get('title'))
             counter += 1
         return movies
 
-    def getSimilarMovies(self, benchmarkMovie):
+    def get_similar_movies(self, benchmark_movie):
         """Given a benchmark movie, return a list of movies that are similar."""
 
-        similarMovies = []
+        similar_movies = []
         # First, get the id of the movie given the name.
-        benchmarkMovieIdResult = requests.get(
-            MOVIE_SEARCH_URL.format(MOVIE_DB_API_KEY, urllib.quote_plus(benchmarkMovie)))
-        benchmarkMovieIdInfo = json.loads(benchmarkMovieIdResult.text)
-        if len(benchmarkMovieIdInfo.get('results')) > 0:
-            benchmarkMovieId = benchmarkMovieIdInfo.get('results')[0].get('id')
+        benchmark_movie_id_result = requests.get(
+            MOVIE_SEARCH_URL.format(
+                MOVIE_DB_API_KEY,
+                urllib.quote_plus(benchmark_movie)))
+        benchmark_movie_id_info = json.loads(benchmark_movie_id_result.text)
+        if len(benchmark_movie_id_info.get('results')) > 0:
+            benchmark_movie_id = benchmark_movie_id_info.get('results')[
+                0].get('id')
             # Get the similar movies given the movie id.
-            movieSimilarityResult = requests.get(
-                MOVIE_SIMILARITY_URL.format(benchmarkMovieId, MOVIE_DB_API_KEY))
-            movieSimilarityInfo = json.loads(movieSimilarityResult.text)
-            if len(movieSimilarityInfo.get('results')) > 0:
-                similarMovies.append(movieSimilarityInfo.get('results')[0].get('title'))
-        return similarMovies
+            movie_similarity_request = requests.get(
+                MOVIE_SIMILARITY_URL.format(
+                    benchmark_movie_id, MOVIE_DB_API_KEY))
+            movie_similarity_info = json.loads(movie_similarity_request.text)
+            if len(movie_similarity_info.get('results')) > 0:
+                similar_movies.append(
+                    movie_similarity_info.get('results')[0].get('title'))
+        return similar_movies
 
-    def encodeURLKeyValue(self, pair):
+    def encode_url_key_value(self, pair):
         """Takes a tuple consisting of a key and a value and encodes it in a format
         that is acceptable for a url string. This is useful for appending
         parameters and their values to an API url."""
         if len(pair[1]) == 0 or len(pair[0]) == 0:
             return ""
         else:
-            if type(pair[1]) == list:
+            if isinstance(pair[1], list):
                 return '&' + pair[0] + '=' + ','.join(str(i) for i in pair[1])
             else:
                 return '&' + pair[0] + '=' + pair[1]
@@ -267,11 +321,11 @@ class LearningAgentClient:
 
     """This class abstracts API calls to our learning agent on Azure."""
 
-    def getRecommendedMovies(self, data):
+    def get_recommended_movies(self, data):
         result = requests.post(LEARNING_AGENT_REC_URL, json=data, auth=(
             "movierecommender", "vast_seas_of_infinity"), verify=False)
         return result.json()
 
-    def addMovieToUserHistory(self, data):
+    def add_movie_to_user_history(self, data):
         requests.post(LEARNING_AGENT_HIST_URL, json=data, auth=(
             "movierecommender", "vast_seas_of_infinity"), verify=False)
